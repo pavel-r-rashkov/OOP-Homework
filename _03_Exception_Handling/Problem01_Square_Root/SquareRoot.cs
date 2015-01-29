aeeb9d0b9b31da81e8027a14cb0060672d16372e
Pavel Rashkov <pavel.r.rashkov@gmail.com>
2015-01-29 19:13:50 +0200

    add homework 03 - Exception handling

---
+++ _03_Exception_Handling/Problem01_Square_Root/SquareRoot.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01_Square_Root
+{
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                int number = int.Parse(input);
+                if(number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Number cannot be null");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Number cannot be less than 0");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Overflow");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Number is not in the correct format");
+            }  
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
---
+++ _03_Exception_Handling/Problem02_Enter_Numbers/EnterNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem02_Enter_Numbers
+{
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            int? lastNumber = null;
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    int currentNumber = ReadNumber(10, 50);
+                    if(lastNumber != null && currentNumber <= lastNumber)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    lastNumber = currentNumber;
+                }                   
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Number cannot be null, enter again:");
+                    i--;
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine("Number out of range, enter again:");
+                    i--;
+                }   
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Overflow, enter again:");
+                    i--;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Number is not in the correct format, enter again:");
+                    i--;
+                }                   
+            }
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            if (number < start || number > end)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return number;
+        }
+    }
+}
